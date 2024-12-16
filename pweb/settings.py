02ab1feee800d1f544f5741151c91b6a00b36631
Pedro-Arthur-GIT <pedroansw@gmail.com>
2024-12-15 23:32:42 -0300

    Added database

--- pweb/settings.py
+++ pweb/settings.py
@@ -79,11 +79,12 @@ WSGI_APPLICATION = 'pweb.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.parse('postgres://neondb_owner:36GhjDYJutVF@ep-old-snow-a4ztagz7-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require')
     }
-}
+
+
+
+
 
 
 
