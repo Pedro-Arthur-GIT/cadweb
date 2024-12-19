b5388bbc00a6289c565cd952ee7518bd32a2b8fd
Pedro-Arthur-GIT <pedroansw@gmail.com>
2024-12-19 15:59:20 -0300

    Added messages

--- home/forms.py
+++ home/forms.py
@@ -12,8 +12,33 @@ class CategoriaForm(forms.ModelForm):
             'nome':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
             'ordem':forms.NumberInput(attrs={'class': 'inteiro form-control', 'placeholder': ''}),
         }
+    def clean_nome(self):
+            nome = self.cleaned_data.get('nome')
+            if len(nome) < 3:
+                raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
+            return nome  
         
+    def clean_ordem(self):
+        ordem = self.cleaned_data.get('ordem')
+        if ordem <= 0:
+            raise forms.ValidationError("O campo ordem deve ser maior que zero.")
+        return ordem    
+       
+    def clean(self):
+         cleaned_data = super().clean()
+         senha = cleaned_data.get("senha")
+         confirmar_senha = cleaned_data.get("confirmar_senha")
 
+         if senha != confirmar_senha:
+              raise forms.ValidationError("As senhas não correspondem")
+         
+    def validar_nome(valor):
+         if len(valor)< 3:
+              raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
+         
+
+
+    
 
 
 
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.contrib import messages
 from .models import *
 from .forms import *
 
@@ -26,6 +27,7 @@ def form_categoria(request):
        form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
        if form.is_valid():# faz a validação do formulário
             form.save() # salva a instancia do modelo no banco de dados
+            messages.success(request, "Operação realizada com sucesso")
             return redirect('categoria') # redireciona para a listagem
     else:# método é get, novo registro
         form = CategoriaForm() # formulário vazio
@@ -34,26 +36,49 @@ def form_categoria(request):
     }
     return render(request, 'categoria/formulario.html', contexto)
 
+
+
+
 def editar_categoria(request, id):
-    categoria = Categoria.objects.get(pk=id)
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+        return redirect('categoria')  # Redireciona para a listagem
+
     if request.method == 'POST':
         # combina os dados do formulário submetido com a instância do objeto existente, permitindo editar seus valores.
         form = CategoriaForm(request.POST, instance=categoria)
         if form.is_valid():
             categoria = form.save() # save retorna o objeto salvo
+            messages.success(request, "Operação realizada com sucesso")
             return redirect('categoria') # redireciona para a listagem
+
     else:
          form = CategoriaForm(instance=categoria)
     return render(request, 'categoria/editar_categoria.html', {'form': form, 'categoria': categoria})
 
 def detalhes_categoria(request, id):
-    categoria = Categoria.objects.get(pk=id)
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+          # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+        return redirect('categoria')  # Redireciona para a listagem
     return render(request, 'categoria/detalhes_categoria.html', {'categoria': categoria})
 
 def excluir_categoria(request, id):
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+          # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+        return redirect('categoria')  # Redireciona para a listagem
     if request.method == 'POST':
         categoria = Categoria.objects.get(id=id)
         categoria.delete()
+        messages.success(request, "Operação realizada com sucesso")
         return redirect('categoria')
     else:
         categoria = Categoria.objects.get(pk=id)
